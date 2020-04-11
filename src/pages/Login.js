7cb5568082ab70c933f4cf73d611cab949343c2a
JOSÃ‰ MANUEL GRANDE ELENES <2017030727@upsin.edu.mx>
2020-04-10 19:29:06 -0600

    register/login validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,48 +8,53 @@ export default class Login extends React.Component {
     super(props);
     this.state = {
       email: "",
-      password: ""
+      password: "",
     };
   }
 
-  signIn = async e => {
+  signIn = async (e) => {
     e.preventDefault();
-    let fd = new FormData();
-    fd.append("email", this.state.email);
-    fd.append("password", this.state.password);
-    const bearer =
-      "Bearer " +
-      "S<yus%;|ZO'1k/ISa^H+6_,!:&$0Z+kM9)B?;f`=]=p%q!)uJ^x_!F!7!LL&F|B";
-    const requestOptions = {
-      method: "POST",
-      body: fd,
-      withCredentials: true,
-      headers: {
-        Authorization: bearer
-      }
-    };
-    try {
-      const response = await fetch(
-        "http://3.219.6.57:5000/login",
-        requestOptions
-      );
-      const respuesta = await response.json();
-      if (respuesta.content.token) {
-        localStorage.setItem("token", respuesta.content.token);
-        swal({
-          icon: "success"
-        });
-        this.props.history.push("/Index");
-      }
-    } catch (err) {
-      swal({
-        icon: "error"
+    if (this.state.email === "" || this.state.password === "") {
+      swal("Llene todos los campos!", {
+        buttons: false,
+        timer: 2000,
       });
-      console.log(err);
+    } else {
+      let fd = new FormData();
+      fd.append("email", this.state.email);
+      fd.append("password", this.state.password);
+      const bearer =
+        "Bearer " +
+        "S<yus%;|ZO'1k/ISa^H+6_,!:&$0Z+kM9)B?;f`=]=p%q!)uJ^x_!F!7!LL&F|B";
+      const requestOptions = {
+        method: "POST",
+        body: fd,
+        withCredentials: true,
+        headers: {
+          Authorization: bearer,
+        },
+      };
+      try {
+        const response = await fetch(
+          "http://3.219.6.57:5000/login",
+          requestOptions
+        );
+        const respuesta = await response.json();
+        if (respuesta.content.token) {
+          localStorage.setItem("token", respuesta.content.token);
+          swal("Hecho!", `${respuesta.details}`, "success");
+          this.props.history.push("/Index");
+        }
+      } catch (err) {
+        swal({
+          icon: "error",
+        });
+        console.log(err);
+      }
     }
   };
 
-  changeHandler = e => {
+  changeHandler = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../includes/bootstrap";
+import swal from "sweetalert";
 
 export default class Register extends React.Component {
   constructor(props) {
@@ -9,15 +10,60 @@ export default class Register extends React.Component {
       name: "",
       surname: "",
       email: "",
-      password: ""
+      password: "",
     };
   }
 
-  signUp = async e => {
+  signUp = async (e) => {
     e.preventDefault();
+    let fd = new FormData();
+    if (
+      this.state.email === "" ||
+      this.state.password === "" ||
+      this.state.name === "" ||
+      this.state.surname === ""
+    ) {
+      swal("Llene todos los campos!", {
+        buttons: false,
+        timer: 2000,
+      });
+    } else {
+      fd.append("name", this.state.name);
+      fd.append("surname", this.state.surname);
+      fd.append("email", this.state.email);
+      fd.append("password", this.state.password);
+      const bearer =
+        "Bearer " +
+        "S<yus%;|ZO'1k/ISa^H+6_,!:&$0Z+kM9)B?;f`=]=p%q!)uJ^x_!F!7!LL&F|B";
+      const requestOptions = {
+        method: "POST",
+        body: fd,
+        withCredentials: true,
+        headers: {
+          Authorization: bearer,
+        },
+      };
+      try {
+        const response = await fetch(
+          "http://3.219.6.57:5000/system/accounts",
+          requestOptions
+        );
+        const respuesta = await response.json();
+        if (respuesta.content.token) {
+          localStorage.setItem("token", respuesta.content.token);
+          swal("Hecho!", `${respuesta.details}`, "success");
+          this.props.history.push("/Login");
+        }
+      } catch (err) {
+        swal({
+          icon: "error",
+        });
+        console.log(err);
+      }
+    }
   };
 
-  changeHandler = e => {
+  changeHandler = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
