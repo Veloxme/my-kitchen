52709eaabb579a6c103cbf1b9123f609572afd3d
JOSÃ‰ MANUEL GRANDE ELENES <2017030727@upsin.edu.mx>
2020-04-13 14:30:15 -0600

    pagination

--- src/componentes/ListaProducts.js
+++ src/componentes/ListaProducts.js
@@ -7,7 +7,12 @@ export default class ListaProducts extends React.Component {
   state = {
     loading: false,
     error: null,
-    Recipes: []
+    Recipes: [],
+    mostrar: [],
+    inicio: 0,
+    final: 10,
+    numero: 0,
+    tope: 0,
   };
   componentDidMount() {
     this.fetchCaregories();
@@ -24,8 +29,8 @@ export default class ListaProducts extends React.Component {
       method: "GET",
       withCredentials: true,
       headers: {
-        Authorization: bearer
-      }
+        Authorization: bearer,
+      },
     };
     try {
       const response = await fetch(
@@ -33,33 +38,123 @@ export default class ListaProducts extends React.Component {
         requestOptions
       );
       const reci = await response.json();
-      const recipes = reci.content;
+      const Recipes = reci.content;
+      const numero = Recipes.length - 10;
       this.setState({
         loading: false,
-        Recipes: recipes
+        Recipes: Recipes,
+        numero,
+      });
+      let algo = [];
+      for (var i = this.state.inicio; i < this.state.final; i++) {
+        algo.push(this.state.Recipes[i]);
+      }
+      this.setState({
+        mostrar: algo,
       });
     } catch (error) {
       this.setState({
         loading: false,
-        error: error
+        error: error,
       });
       swal({
-        icon: "error"
+        icon: "error",
       });
     }
   };
-  put = async e => {
+  paginationplus = () => {
+    let algo = [];
+    if (this.state.numero >= 10) {
+      let inicio = this.state.inicio + 10;
+      let final = this.state.final + 10;
+      let restar = this.state.numero - 10;
+      this.setState({
+        inicio,
+        final,
+        numero: restar,
+      });
+
+      for (var i = inicio; i < final; i++) {
+        algo.push(this.state.Recipes[i]);
+      }
+      this.setState({
+        mostrar: algo,
+      });
+    } else if (this.state.tope === 1 || this.state.numero === 0) {
+      swal("No se puede", {
+        buttons: false,
+        timer: 2000,
+      });
+    } else {
+      let inicio = this.state.inicio + 10;
+      let final = this.state.final + this.state.numero;
+      this.setState({
+        inicio,
+        final,
+        tope: 1,
+      });
+
+      for (var x = inicio; x < final; x++) {
+        algo.push(this.state.Recipes[x]);
+      }
+      this.setState({
+        mostrar: algo,
+      });
+    }
+  };
+  paginationmenus = () => {
+    if (this.state.inicio > 0) {
+      if (this.state.tope === 1) {
+        let algo = [];
+        let inicio = this.state.inicio - 10;
+        let final = this.state.final - this.state.numero;
+        this.setState({
+          inicio,
+          final,
+          tope: 0,
+        });
+        for (var i = inicio; i < final; i++) {
+          algo.push(this.state.Recipes[i]);
+        }
+        this.setState({
+          mostrar: algo,
+        });
+      } else {
+        let algo = [];
+        let inicio = this.state.inicio - 10;
+        let final = this.state.final - 10;
+        let numero = this.state.numero + 10;
+        this.setState({
+          inicio,
+          final,
+          numero,
+        });
+        for (var x = inicio; x < final; x++) {
+          algo.push(this.state.Recipes[x]);
+        }
+        this.setState({
+          mostrar: algo,
+        });
+      }
+    } else {
+      swal("No se puede", {
+        buttons: false,
+        timer: 2000,
+      });
+    }
+  };
+  put = async (e) => {
     this.props.history.push(`/Index/EditProduct/${e}`);
   };
-  delete = async e => {
+  delete = async (e) => {
     this.setState({ loading: true });
     const bearer = "Bearer " + localStorage.getItem("token");
     const requestOptions = {
       method: "DELETE",
       withCredentials: true,
       headers: {
-        Authorization: bearer
-      }
+        Authorization: bearer,
+      },
     };
     try {
       await fetch(`http://3.219.6.57:5000/admin/product/${e}`, requestOptions);
@@ -69,10 +164,10 @@ export default class ListaProducts extends React.Component {
       this.fetchCaregories();
     } catch (err) {
       this.setState({
-        loading: false
+        loading: false,
       });
       swal({
-        icon: "error"
+        icon: "error",
       });
       console.log(err);
     }
@@ -80,8 +175,23 @@ export default class ListaProducts extends React.Component {
   render() {
     return (
       <div className="container mt-3 mb-3">
-        <ul className="list-group">
-          {this.state.Recipes.map(rec => (
+        <nav aria-label="Page navigation  ">
+          <ul className="pagination justify-content-center">
+            <li className="page-item ">
+              <span className="page-link" onClick={this.paginationmenus}>
+                Previous
+              </span>
+            </li>
+
+            <li className="page-item">
+              <span className="page-link" onClick={this.paginationplus}>
+                Next
+              </span>
+            </li>
+          </ul>
+        </nav>
+        <ul className="list-group mt-3">
+          {this.state.mostrar.map((rec) => (
             <li className="list-group-item" key={rec.id}>
               {rec.name}
               <button
